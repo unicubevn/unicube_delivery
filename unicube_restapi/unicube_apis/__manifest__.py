52bdf41d7b19f346783836b0ce6a10780f60d831
Jean Nguyen <unicube.vn@outlook.com>
2024-02-29 07:32:10 +0700

    update unicube_apis

--- unicube_restapi/unicube_apis/__manifest__.py
+++ unicube_restapi/unicube_apis/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "unicube_apis",
+    "name": "UniCube OpenApi",
 
-    "summary": "Short (1 phrase/line) summary of the module's purpose",
+    "summary": "UniCube OpenApi for Delivery System",
 
     "description": """
 Long description of module's purpose
