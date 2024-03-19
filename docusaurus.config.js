031ccc720252eb6e6f489285730656655170edff
Jean Nguyen <unicube.vn@outlook.com>
2024-03-20 00:01:59 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://teship.net',
+  url: 'https://delivery-docs.unicube.vn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -29,6 +29,7 @@ const config = {
   organizationName: 'unicubevn', // Usually your GitHub org/user name.
   projectName: 'unicube_delivery', // Usually your repo name.
   deploymentBranch:'documentation',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
