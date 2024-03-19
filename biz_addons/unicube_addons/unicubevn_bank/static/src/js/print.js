1784246ccb114e03d3300c87e1bbace79fd1e379
Jean Nguyen <unicube.vn@outlook.com>
2024-03-19 20:47:26 +0700

    update css and print function for unicubevn_bank

--- biz_addons/unicube_addons/unicubevn_bank/static/src/js/print.js
+++ biz_addons/unicube_addons/unicubevn_bank/static/src/js/print.js
@@ -4,6 +4,8 @@
  * #  These code are maintained by The UniCube.
  */
 
-function print_receipt(){
-    window.print(document.getElementsByClassName('bean_page'))
+function print_receipt(className = 'bean_page') {
+    // console.log("className: ", className)
+    // console.log("className: ", document.getElementsByClassName(className)[0])
+    window.print(document.getElementsByClassName(className)[0])
 }
